bf6c6f6a8ed747657f8feec51051556a346d9760
Fadlan Fajar Ramdani <fdlnfjrrmdni@gmail.com>
2020-08-20 13:11:55 +0700

    err in 2

--- 2/2.go
+++ 2/2.go
@@ -17,7 +17,7 @@ func getValue(str string, i int, m int) int {
 		if (c < 0 || c > 9) {
 			return -1
 		}
-		value = value * 10 + c 
+		value = value * 10 + int(c)
 	} 
 	return value
 }
@@ -40,15 +40,15 @@ func findMissingNumber(str string) int {
 		fail := false
 
 		// Find subsequent numbers with previous number as n 
-		for i:=m; i!=len(str); i += 1 + math.Log10(n) { 
+		for i:=m; i!=len(str); i += 1 + int(math.Log10(float64(n))) { 				///OOOOOOOOOOOO
 			// If we haven't yet found the missing number 
 			// for current length. Next number is n+2. Note 
 			// that we use Log10 as (n+2) may have more 
 			// length than n. 
-			if ((missingNo == -1) && (getValue(str, i, 1+math.Log10(n+2)) == n+2)) { 
+			if ((missingNo == -1) && (getValue(str, i, 1+int(math.Log10(float64(n)+2.))) == n+2)) { 			///OOOOOOOOOOOO
 				missingNo = n + 1
 				n += 2
-			}else if (getValue(str, i, 1+math.Log10(n+1)) == n+1) {	// If next value is (n+1) 
+			}else if (getValue(str, i, 1+int(math.Log10(float64(n)+1.))) == n+1) {	// If next value is (n+1) 		///OOOOOOOOOOOO
 				n++
 			}else { 
 				fail = true
